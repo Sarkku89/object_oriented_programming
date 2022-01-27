a2a5c3bf4598c4395481e9aba5690b87ae20d528
Sarianna Junnila <sarianna.junnila@edu.turkuamk.fi>
2022-01-27 22:00:12 +0200

    Commenting alarm clock program

--- exersice2/alarmClock.py
+++ exersice2/alarmClock.py
@@ -4,6 +4,7 @@
 
 import time
 
+# Initializing the alarm time and current time
 
 class AlarmClock:
     def __init__(self):
@@ -11,9 +12,10 @@ class AlarmClock:
         self.alarm_minutes = int(input("Give the minutes for alarm (MM): "))
         self.alarm_seconds = int(input("Give the seconds for alarm (SS): "))
         self.time_now = time.strftime('%H:%M:%S')
-        
-    def alarm(self):
 
+# Slicing the current time string and comparing it to the alarm time every 1 second
+
+    def alarm(self):
         while True:
             time.sleep(1)
             self.time_now = time.strftime('%H:%M:%S')
@@ -26,8 +28,9 @@ class AlarmClock:
                     if self.seconds == self.alarm_seconds:
                         print ("Time to wake up!")
                         break
-               
-           
+
+# Main function 
+                  
 def main():
     my_alarm = AlarmClock()
     my_alarm.alarm()
