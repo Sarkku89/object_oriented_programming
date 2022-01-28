34269bd443cf6176bfb5f04a84d1d5003bfee542
Sarianna Junnila <sarianna.junnila@edu.turkuamk.fi>
2022-01-28 16:00:59 +0200

    Changing the structure of average grade program

--- exersice2/avarageGrade.py
+++ exersice2/avarageGrade.py
@@ -1,18 +1,31 @@
 #File name: avarageGrade.py
 #Author: Sarianna Junnila
-#Description: A program which gives the user the avarage grade of the course and saves the inputs in a dictionary.
+#Description: A program which gives the user the avarage grade 
+# of the course and saves the inputs in a dictionary.
 
-grade_table = {}
-grade_list = []
-
-def avarage_grade():
-    avarage = sum(grade_list)
+def avar_grade():
+    grade_table = {}
+    grade_list = []
     key = input("Write your name here: ")
     value = int(input("Write your grade here: "))
-    grade_table[key] = value
-    grade_list.append(value)
-    avarage = (value + avarage) / len(grade_list)
-    print ("The average grade for this course is: ", avarage)
-    avarage_grade()
+    if value >= 0 and value <= 5:
+        grade_table[key] = value
+        grade_list.append(value)
+        avarage = sum(grade_list)
+    else:
+        print ("Your grade must be between 0 and 5. Please try again.")
+        return avar_grade()
 
-avarage_grade()
+    while input ("Do you want to add more grades? y/n ") == "y":
+        key = input("Write your name here: ")
+        value = int(input("Write your grade here: "))
+        grade_table[key] = value
+        grade_list.append(value)
+        avarage = sum(grade_list) / len(grade_list)
+
+    if avarage == 0:
+        print ("Sorry, everybody has failed this course. The avarage grade is 0.")
+    else:
+        print ("The average grade for this course is: ", avarage)
+
+avar_grade()
