3d5b308cd44c7d2a1838cae878867ac2740d6ec9
Sarianna Junnila <sarianna.junnila@edu.turkuamk.fi>
2022-01-31 14:45:22 +0200

    Adding options to the Dice game
    Trying to cover all possible result options in every round

--- exercise3/diceGame.py
+++ exercise3/diceGame.py
@@ -2,6 +2,7 @@
 # Author: Sarianna Junnila
 # Description:  
 
+from msilib.schema import ServiceControl
 import random
 
 # Class definition 
@@ -94,95 +95,271 @@ def play_the_game():
     print ("Third player rolled number", third_player_roll)
 
 
-# The rounds with clear winners
+# ----- The first rounds with clear winners -----
 
 # Third player is played out in the first round  
-    if first_player_roll > second_player_roll and second_player_roll > third_player_roll:
+
+    if first_player_roll > third_player_roll and second_player_roll > third_player_roll :
         print ("The Third player is out of the game! First and Second player rematch!")
 
-# Second round with First and Second player
+    # Second round with First and Second player
+        
         first_player_roll = first_player()
         print ("Second round: First player gets:", first_player_roll)
         second_player_roll = second_player()
         print ("Second round: The Second player gets:", second_player_roll)
-        if first_player_roll > second_player_roll:
-            print ("The First player won the game!")
-        if first_player_roll == second_player_roll:
-            print ("Tie! with the number:", first_player_roll, "Roll again!")
-        if second_player_roll > first_player_roll:
-            print ("The Second player won the game!")
+        while True:
+            if first_player_roll > second_player_roll:
+                print ("The First player won the game!")
+                break
+            if second_player_roll > first_player_roll:
+                print ("The Second player won the game!")
+                break
+            if first_player_roll == second_player_roll:
+                print ("Tie! Roll again!")
+                first_player_roll = first_player()
+                print ("Second round: First player gets:", first_player_roll)
+                second_player_roll = second_player()
+                print ("Second round: The Second player gets:", second_player_roll)
 
 
 
-# First player is played out in the first round  
-    elif second_player_roll > first_player_roll and first_player_roll < third_player_roll:
+# First player is played out in the first round 
+
+    elif second_player_roll > first_player_roll and third_player_roll > first_player_roll:
         print ("The First player is out of the game! Second and Third player rematch!")
 
-#Second round with Second and Third player
+    # Second round with Second and Third player
+
         second_player_roll = second_player()
-        print ("Second round: The Second player gets:", second_player_roll)
+        print ("Second round: Second player gets:", second_player_roll)
         third_player_roll = third_player()
         print ("Second round: Third player gets:", third_player_roll)
-        if second_player_roll > third_player_roll:
-            print ("The Second player won the game!")
-        if second_player_roll == third_player_roll:
-            print ("Tie! with the number:", second_player_roll, "Roll again!")
-        if third_player_roll > second_player_roll:
-            print ("The Third player won the game!")
+        while True:
+            if second_player_roll > third_player_roll:
+                print ("The Second player won the game!")
+                break
+            if third_player_roll > second_player_roll:
+                print ("The Third player won the game!")
+                break
+            if second_player_roll == third_player_roll:
+                print ("Tie! Roll again!")
+                second_player_roll = second_player()
+                print ("Second round: The Second player gets:", second_player_roll)
+                third_player_roll = third_player()
+                print ("Second round: Third player gets:", third_player_roll)
 
 
 
-# Second player is played out in the first round
-    elif first_player_roll > second_player_roll and second_player_roll < third_player_roll:
+# Second player is played out in the first round 
+
+    elif first_player_roll > second_player_roll and  third_player_roll > second_player_roll:
         print ("The Second player is out of the game! First and Third player rematch!")
 
-# Second round with first and third player
+    # Second round with first and third player
+
         first_player_roll = first_player()
         print ("Second round: First player gets:", first_player_roll)
         third_player_roll = third_player()
         print ("Second round: Third player gets:", third_player_roll)
-        if first_player_roll > third_player_roll:
-            print ("The First player won the game!")
-        if first_player_roll == third_player_roll:
-            print ("Tie! with the number:", first_player_roll, "Roll again!")
-        if third_player_roll > first_player_roll:
-            print ("The Third player won the game!")
-   
+
+        while True:
+            if first_player_roll > third_player_roll:
+                print ("The First player won the game!")
+                break
+            if third_player_roll > first_player_roll:
+                print ("The Third player won the game!")
+                break
+            if first_player_roll == third_player_roll:
+                print ("Tie! Roll again!")
+                first_player_roll = first_player()
+                print ("Second round: First player gets:", first_player_roll)
+                third_player_roll = third_player()
+                print ("Second round: Third player gets:", third_player_roll)
+
+# ---- If all players get same number in the first round ----
     elif first_player_roll == second_player_roll == third_player_roll:
         print ("All players rolled the same number! Rematch!")
-        return play_the_game()
+        play_the_game()
+
+# ---- If there is only a one winner of first round and tie with 2 dices ----
+
+    elif first_player_roll == second_player_roll or second_player_roll == third_player_roll  or first_player_roll == third_player_roll:
+
+    # Tie between First and Second player
+
+        if first_player_roll == second_player_roll:
+            print ("First and Second player had a tie! Roll again! The Third player is through to next round!")
+            first_player_roll = first_player()
+            print ("First player gets: ", first_player_roll)
+            second_player_roll = second_player ()
+            print ("Second player gets:", second_player_roll)
+
+            while True:
+                if first_player_roll > second_player_roll:
+                    print ("Second player is played out! First and Third player go to Round 2!")
+
+                # Second round with First and Third player
+
+                    first_player_roll = first_player()
+                    print ("Second round: First player gets:", first_player_roll)
+                    third_player_roll = third_player()
+                    print ("Second round: Third player gets:", third_player_roll)
+
+                    while first_player_roll > second_player_roll or second_player_roll > first_player_roll:
+                        if first_player_roll > third_player_roll:
+                            print ("The First player won the game!")
+                            break
+                        if third_player_roll > first_player_roll:
+                            print ("The Third player won the game!")
+                            break
+                        if first_player_roll == third_player_roll:
+                            print ("Tie! Roll again!")
+                            first_player_roll = first_player()
+                            print ("Second round: First player gets:", first_player_roll)
+                            third_player_roll = third_player()
+                            print ("Second round: Third player gets:", third_player_roll)
+
+                if second_player_roll > first_player_roll:
+                    print ("First player is played out! Second and Third player rematch!")
+                
+                # Second round with Second and Third player
+                    second_player_roll = second_player()
+                    print ("Second round: The Second player gets:", second_player_roll)
+                    third_player_roll = third_player()
+                    print ("Second round: Third player gets:", third_player_roll)
+
+                    while second_player_roll > third_player_roll or third_player_roll > second_player_roll:
+                        if second_player_roll > third_player_roll:
+                            print ("The Second player won the game!")
+                            break
+                        if third_player_roll > second_player_roll:
+                            print ("The Third player won the game!")
+                            break
+                        if second_player_roll == third_player_roll:
+                            print ("Tie! Roll again!")
+                            second_player_roll = second_player()
+                            print ("Second round: The Second player gets:", second_player_roll)
+                            third_player_roll = third_player()
+                            print ("Second round: Third player gets:", third_player_roll)
+
+    # Tie between First and Third player
+
+    elif first_player_roll == third_player_roll:
+            print ("First and Third player had a tie! Roll again! The Second player is through to next round!")
+            first_player_roll = first_player()
+            print ("First player gets: ", first_player_roll)
+            third_player_roll = third_player ()
+            print ("Third player gets:", third_player_roll)
+
+            while True :
+                if first_player_roll > third_player_roll:
+                    print ("Third player is played out! First and Second player go to Round 2!")
+
+                # Second round with First and Second player
+
+                    first_player_roll = first_player()
+                    print ("Second round: First player gets:", first_player_roll)
+                    second_player_roll = second_player()
+                    print ("Second round: Second player gets:", second_player_roll)
+
+                    while first_player_roll > second_player_roll or second_player_roll > first_player_roll:
+                        if first_player_roll > third_player_roll:
+                            print ("The First player won the game!")
+                            break
+                        if second_player_roll > first_player_roll:
+                            print ("The Third player won the game!")
+                            break
+                        if first_player_roll == second_player_roll:
+                            print ("Tie! Roll again!")
+                            first_player_roll = first_player()
+                            print ("Second round: First player gets:", first_player_roll)
+                            second_player_roll = second_player()
+                            print ("Second round: Second player gets:", second_player_roll)
+
+                if second_player_roll > first_player_roll:
+                    print ("First player is played out! Second and Third player rematch!")
+                
+                # Second round with Second and Third player
+                    second_player_roll = second_player()
+                    print ("Second round: The Second player gets:", second_player_roll)
+                    third_player_roll = third_player()
+                    print ("Second round: Third player gets:", third_player_roll)
+
+                    while second_player_roll > third_player_roll or third_player_roll > second_player_roll:
+                        if second_player_roll > third_player_roll:
+                            print ("The Second player won the game!")
+                            break
+                        if third_player_roll > second_player_roll:
+                            print ("The Third player won the game!")
+                            break
+                        if second_player_roll == third_player_roll:
+                            print ("Tie! Roll again!")
+                            second_player_roll = second_player()
+                            print ("Second round: The Second player gets:", second_player_roll)
+                            third_player_roll = third_player()
+                            print ("Second round: Third player gets:", third_player_roll)
+            
+    # Tie between Second and Third player
+
+    elif second_player_roll == third_player_roll:
+            print ("Second and Third player had a tie! Roll again! The First player is through to next round!")
+            second_player_roll = second_player()
+            print ("Second player gets: ", first_player_roll)
+            third_player_roll = third_player ()
+            print ("Third player gets:", third_player_roll)
+
+            while True:
+                if second_player_roll > third_player_roll:
+                    print ("Third player is played out! First and Second player go to Round 2!")
+
+                # Second round with First and Second player
+
+                    first_player_roll = first_player()
+                    print ("Second round: First player gets:", first_player_roll)
+                    second_player_roll = second_player()
+                    print ("Second round: Second player gets:", second_player_roll)
+
+                    while first_player_roll > second_player_roll or second_player_roll > first_player_roll:
+                        if first_player_roll > third_player_roll:
+                            print ("The First player won the game!")
+                            break
+                        if second_player_roll > first_player_roll:
+                            print ("The Third player won the game!")
+                            break
+                        if first_player_roll == second_player_roll:
+                            print ("Tie! Roll again!")
+                            first_player_roll = first_player()
+                            print ("Second round: First player gets:", first_player_roll)
+                            second_player_roll = second_player()
+                            print ("Second round: Second player gets:", second_player_roll)
+
+                if third_player_roll > second_player_roll:
+                    print ("Second player is played out! First and Third player rematch!")
+                
+                # Second round with First and Third player
+
+                    first_player_roll = first_player()
+                    print ("Second round: First player gets:", first_player_roll)
+                    third_player_roll = third_player()
+                    print ("Second round: Third player gets:", third_player_roll)
+
+                    while first_player_roll > second_player_roll or second_player_roll > first_player_roll:
+                        if first_player_roll > third_player_roll:
+                            print ("The First player won the game!")
+                            break
+                        if third_player_roll > first_player_roll:
+                            print ("The Third player won the game!")
+                            break
+                        if first_player_roll == third_player_roll:
+                            print ("Tie! Roll again!")
+                            first_player_roll = first_player()
+                            print ("Second round: First player gets:", first_player_roll)
+                            third_player_roll = third_player()
+                            print ("Second round: Third player gets:", third_player_roll)
+
+
 
 play_the_game()
 # Ties in the first round:
     
-""" if first_player_roll == second_player_roll:
-        print ("Tie with First and Second player! They roll the dice again!")
-        first_player_roll = first_player()
-        second_player_roll = second_player()
-        if first_player_roll > second_player_roll:
-            print ("The First player won the rematch with number", first_player_roll)
-        else:
-            print ("The Second player won the rematch with number", second_player_roll)
-
-    if second_player_roll == third_player_roll:
-        print ("Tie with second and third player! They roll the dice again!")
-        second_player_roll = second_player()
-        third_player_roll = third_player()
-        if second_player_roll > third_player_roll:
-            print ("The second player won the rematch with number", second_player_roll)
-        else:
-            print ("The third player won the rematch with number", third_player_roll)
-
-    if first_player_roll == third_player_roll:
-        print ("Tie with first and third player! They roll the dice again!")
-        first_player_roll = first_player()
-        third_player_roll = third_player()
-        if first_player_roll > third_player_roll:
-            print ("The first player won the rematch with number", first_player_roll)
-        else:
-            print ("The third player won the rematch with number", third_player_roll)"""
-
-
-
-        
-        
